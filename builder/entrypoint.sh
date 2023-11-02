f606d8345e0a607fd90f13e3a78f00aca53a37f5
Ricardo Santos <ricardo.santos9521@outlook.com>
2023-11-02 17:26:18 +0000

    add .k8s deployment

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -12,7 +12,7 @@ check_vars()
 
 check_vars DOCKER_REGISTRY_URL DOCKER_REGISTRY_USERNAME DOCKER_REGISTRY_TOKEN GITHUB_ACTIONS_URL GITHUB_ACTIONS_TOKEN GITHUB_ACTIONS_AGENT_NAME
 
-cd .actions-runner/
+cd ~/.actions-runner/
 
 #check docker credential
 expected_registry=$DOCKER_REGISTRY_URL
@@ -22,8 +22,7 @@ credentials=""
 
 if [ -e ~/.docker/config.json ]; then
     credentials=$(jq -r '.auths | to_entries[] | "\(.key) \(.value.auth)"' ~/.docker/config.json)
-fi
-
+fi &&
 if [ -n "$credentials" ]; then
     registry=$(echo "$credentials" | cut -d' ' -f1)
     username=$(echo "$credentials" | cut -d' ' -f2 | base64 --decode | cut -d':' -f1)
@@ -37,13 +36,10 @@ if [ -n "$credentials" ]; then
     fi
 else
     docker login $DOCKER_REGISTRY_URL -u $DOCKER_REGISTRY_USERNAME -p $DOCKER_REGISTRY_TOKEN; 
-fi
-
-
+fi &&
 #confirm if svc.sh already exists if exists it was already configures
 if [ ! -e svc.sh ]; then 
     ./config.sh --replace --url $GITHUB_ACTIONS_URL --token $GITHUB_ACTIONS_TOKEN --name $GITHUB_ACTIONS_AGENT_NAME --labels $GITHUB_ACTIONS_AGENT_NAME --work /home/useragent/_work --unattended; 
-fi
-
+fi &&
 #run actions
 ./run.sh
--- deployer/entrypoint.sh
+++ deployer/entrypoint.sh
@@ -12,13 +12,11 @@ check_vars()
 
 check_vars KUBE_CONFIG GITHUB_ACTIONS_URL GITHUB_ACTIONS_TOKEN GITHUB_ACTIONS_AGENT_NAME
 
-cd .actions-runner/
+cd ~/.actions-runner/
 #decode KUBE_CONFIG
-echo $KUBE_CONFIG | base64 --decode > ~/.kube/config
-
+echo "$KUBE_CONFIG" > ~/.kube/config &&
 #confirm if svc.sh already exists if exists it was already configures
 if [ ! -e svc.sh ]; then 
     ./config.sh --replace --url $GITHUB_ACTIONS_URL --token $GITHUB_ACTIONS_TOKEN --name $GITHUB_ACTIONS_AGENT_NAME --labels $GITHUB_ACTIONS_AGENT_NAME --work /home/useragent/_work --unattended;
-fi
-
+fi &&
 ./run.sh
