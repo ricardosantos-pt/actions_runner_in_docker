d6036a51b05a1dd4f49192fc1a58accf27668a8b
Ricardo Santos <ricardo.santos9521@outlook.com>
2023-11-04 13:59:50 +0000

    update entrypoint

--- deployer/entrypoint.sh
+++ deployer/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd ~/.actions-runner/
-
 echo "--------------------------------$(date)--------------------------------"
 
 check_vars()
@@ -16,10 +14,46 @@ check_vars()
 
 check_vars KUBE_CONFIG GITHUB_ACTIONS_URL GITHUB_ACTIONS_TOKEN GITHUB_ACTIONS_AGENT_NAME
 
+cache_folder=~/.cache_volume
+cache_folder_exists="false"
+if [[ -d "$cache_folder" ]]; then
+  cache_folder_exists="true"
+  if [[ -d "$cache_folder/docker_cache/" ]]; then
+    echo "copying cache docker"
+    mkdir -p ~/.docker
+    cd "$cache_folder/docker_cache"
+    files_to_cache=$(find . -type f)
+    rsync -av --files-from=<(echo "$files_to_cache") "$cache_folder/docker_cache/" ~/.docker
+  fi
+  if [[ -d "$cache_folder/github_actions_cache/" ]]; then
+    echo "copying cache actions runner"
+    cd "$cache_folder/github_actions_cache"
+    files_to_cache=$(find . -type f)
+    rsync -av --files-from=<(echo "$files_to_cache") "$cache_folder/github_actions_cache/" ~/.actions-runner
+  fi
+fi &&
 #decode KUBE_CONFIG
 echo "$KUBE_CONFIG" > ~/.kube/config &&
 #confirm if svc.sh already exists if exists it was already configures
-if [ ! -e svc.sh ]; then 
-    ./config.sh --replace --url $GITHUB_ACTIONS_URL --token $GITHUB_ACTIONS_TOKEN --name $GITHUB_ACTIONS_AGENT_NAME --labels $GITHUB_ACTIONS_AGENT_NAME --work /home/useragent/_work --unattended;
+if [ ! -e ~/.actions-runner/svc.sh ]; then 
+    cd ~/.actions-runner/
+
+    if [[ "$cache_folder_exists" == "true" ]]; then
+        find . -type f -exec md5sum {} \; > before.md5
+    fi
+
+    ./config.sh --replace --url $GITHUB_ACTIONS_URL --token $GITHUB_ACTIONS_TOKEN --name $GITHUB_ACTIONS_AGENT_NAME --labels $GITHUB_ACTIONS_AGENT_NAME --work /home/useragent/_work --unattended; 
+    pid=$!
+    wait $pid
+
+    if [[ "$cache_folder_exists" == "true" ]]; then
+        echo "Caching github runner"
+        find . -type f -exec md5sum {} \; | grep -E -v 'before.md5|after.md5' > after.md5
+        files_changed=$(diff before.md5 after.md5 | awk '$1 == ">" {print $3}')
+        mkdir -p "$cache_folder/github_actions_cache/"
+        rsync -av --files-from=<(echo "$files_changed") ~/.actions-runner/ "$cache_folder/github_actions_cache/"
+        rm -f before.md5 after.md5
+    fi
 fi &&
-./run.sh
+#run actions
+~/.actions-runner/run.sh
